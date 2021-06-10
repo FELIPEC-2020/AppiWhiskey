6be9f6b47c86e12c13c0fbbd1ab65ff7261f9e39
FELIPEC-2020 <andres.3801911026@ucaldas.edu.co>
2021-06-09 22:50:26 -0500

    Muestra lista destileria

--- app/src/main/java/com/ch/appiwhiskey/DestileriaAdapter.java
+++ app/src/main/java/com/ch/appiwhiskey/DestileriaAdapter.java
@@ -1,4 +1,52 @@
 package com.ch.appiwhiskey;
 
-public class DestileriaAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.ch.appiwhiskey.models.Destileria;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class DestileriaAdapter extends BaseAdapter {
+    protected Activity activity;
+    protected List<Destileria> myDestileria;
+
+    public DestileriaAdapter(Activity activity, List<Destileria> myDestileria) {
+        this.activity = activity;
+        this.myDestileria = myDestileria;
+    }
+
+    @Override
+    public int getCount() {
+        return myDestileria.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return myDestileria.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vista = convertView;
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            vista = inflater.inflate(R.layout.destileria_items, null);
+        }
+        Destileria objDestileria = myDestileria.get(position);
+        TextView nombreDestileria = vista.findViewById(R.id.txtNombreDestileria);
+        nombreDestileria.setText(objDestileria.getDestileriaName());
+        return vista;
+    }
 }
--- app/src/main/java/com/ch/appiwhiskey/Destilerias.java
+++ app/src/main/java/com/ch/appiwhiskey/Destilerias.java
@@ -3,12 +3,43 @@ package com.ch.appiwhiskey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.ch.appiwhiskey.ApiManager.RetrofitClient;
+import com.ch.appiwhiskey.models.Destileria;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Destilerias extends AppCompatActivity {
+    ListView lvDestileria;
+    List<Destileria> listaDestilerias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destilerias);
+        lvDestileria = findViewById(R.id.lvDestileria);
+        getDestileriasFromApi();
+
+    }
+    private void getDestileriasFromApi(){
+        Call<List<Destileria>> call = RetrofitClient.getInstance().getMyApi().getDestilerias();
+        call.enqueue(new Callback<List<Destileria>>() {
+            @Override
+            public void onResponse(Call<List<Destileria>> call, Response<List<Destileria>> response) {
+                listaDestilerias = response.body();
+                DestileriaAdapter adapter = new DestileriaAdapter(Destilerias.this, listaDestilerias);
+                lvDestileria.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Destileria>> call, Throwable t) {
+
+            }
+        });
     }
 }
